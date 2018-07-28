f08b74a9e8090a1506136825083e79e6b9526e34
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-28 10:03:29 -0400

    Datos

---
+++ Practica2.2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n1, n2, n3;
+			Console.WriteLine("Introducir primer numero>");
+			n1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Introducir segundo numero:");
+			n2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Introducir tercer numero:");
+			n3 = Convert.ToInt32(Console.ReadLine());
+
+			if (n1 < n2 && n1 < n3)
+			{
+				if (n2 < n3)
+				{
+					Console.WriteLine("Primer numero:" + n1);
+					Console.WriteLine("Primer numero:" + n2);
+					Console.WriteLine("Primer numero:" + n3);
+				}
+				else
+				{
+					Console.WriteLine("Primer numero:" + n1);
+					Console.WriteLine("Primer numero:" + n3);
+					Console.WriteLine("Primer numero:" + n2);
+				}
+
+				if (n2 < n1 && n2 < n3)
+				{
+					if (n1 < n3)
+					{
+						Console.WriteLine("Primer numero:" + n2);
+						Console.WriteLine("Primer numero:" + n1);
+						Console.WriteLine("Primer numero:" + n3);
+					}
+
+					else
+					{
+						Console.WriteLine("Primer numero:" + n2);
+						Console.WriteLine("Primer numero:" + n3);
+						Console.WriteLine("Primer numero:" + n1);
+					}
+
+					if (n3 < n1 && n3 < n2)
+					{
+						if (n1 < n2)
+						{
+							Console.WriteLine("Primer numero:" + n3);
+							Console.WriteLine("Primer numero:" + n1);
+							Console.WriteLine("Primer numero:" + n2);
+						}
+
+						else
+						{
+							Console.WriteLine("Primer numero:" + n3);
+							Console.WriteLine("Primer numero:" + n2);
+							Console.WriteLine("Primer numero:" + n1);
+						}
+					}
+
+						}
+				Console.ReadKey();
+			}
+		}
+	}
+}
